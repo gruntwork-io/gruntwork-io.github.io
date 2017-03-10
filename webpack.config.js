18ad227792843b5f6c654150f15b9b035bd30f1d
Kostas Bariotis <konmpar@gmail.com>
2017-03-10 17:17:55 +0200

    disable CORS for dev server

--- webpack.config.js
+++ webpack.config.js
@@ -84,4 +84,11 @@ module.exports = {
     }),
   ],
   devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    }
+  },
 };
