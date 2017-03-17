1046fce0dd0f8327035e5ab8ba0ea27e1fa41cb3
Kostas Bariotis <konmpar@gmail.com>
2017-03-17 15:38:33 +0200

    finalize infrastructure packages page

--- source/js/infra-page.js
+++ source/js/infra-page.js
@@ -1,3 +1,20 @@
-var accordion = require('./utils/accordion');
+var moreInfoButtons = document.querySelectorAll('.package__cta');
 
-accordion();
+for (i = 0; i < moreInfoButtons.length; i++) {
+  moreInfoButtons[i].addEventListener('click', toggleItem1, false);
+}
+
+function toggleItem1 (e) {
+  e.preventDefault();
+
+  var package = this.parentNode.parentNode;
+
+  var textContainer = package.querySelector('.package__text');
+  if (textContainer.style.maxHeight === '100%') {
+    this.innerHTML = 'More info'
+    textContainer.style.maxHeight = '50px';
+  } else {
+    this.innerHTML = 'Less info'
+    textContainer.style.maxHeight = '100%';
+  }
+}
