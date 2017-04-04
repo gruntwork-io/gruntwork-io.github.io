239b42b25a5494da4a0b9d2fc42d53ff72ca9565
Kostas Bariotis <konmpar@gmail.com>
2017-04-04 14:21:57 +0300

    rebuild assets

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,2 @@
-webpackJsonp([3],{31:function(module,exports){eval("// removed by extract-text-webpack-plugin//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMzEuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zb3VyY2Uvc2Fzcy9tYWluLnNjc3M/NWQwYyJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyByZW1vdmVkIGJ5IGV4dHJhY3QtdGV4dC13ZWJwYWNrLXBsdWdpblxuXG5cbi8vLy8vLy8vLy8vLy8vLy8vL1xuLy8gV0VCUEFDSyBGT09URVJcbi8vIC4vc291cmNlL3Nhc3MvbWFpbi5zY3NzXG4vLyBtb2R1bGUgaWQgPSAzMVxuLy8gbW9kdWxlIGNodW5rcyA9IDMiXSwibWFwcGluZ3MiOiJBQUFBIiwic291cmNlUm9vdCI6IiJ9")}},[31]);
+webpackJsonp([3],{31:function(n,c){}},[31]);
+//# sourceMappingURL=main.js.map
