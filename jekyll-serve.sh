f6de07400d92a064e640ddb0bfb589166a98db56
Sahil Saini <sahil@akosweb.com>
2018-09-24 10:58:01 -0700

    elaborate on terminal echos

--- jekyll-serve.sh
+++ jekyll-serve.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+echo -e "\e[1;31mBuild Jekyll for Pandoc to make new ToS Docx"
 bundle exec jekyll build
-bundle exec pandoc -s --from html _site/terms/word.html --reference-doc=reference.docx -o Gruntwork-TermsOfService.docx && echo -e "\e[1;31mNew TOS Document (Gruntwork-TermsOfService.docx) File Generated"
+bundle exec pandoc -s --from html _site/terms/word.html --reference-doc=reference.docx -o Gruntwork-TermsOfService.docx
+echo -e "\e[1;32mNew TOS Document (Gruntwork-TermsOfService.docx) File Generated"
+echo -e "\e[1;31mRun Jekyll serve to watch for changes"
 bundle exec jekyll serve --drafts --incremental --host 0.0.0.0
