913d672f58fd9077e058d141368f309bc6a006ae
Josh Padnick <josh.padnick@gmail.com>
2019-02-14 14:02:42 -0700

    Correct issue with timezone offsets.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -9,7 +9,12 @@
   // calculate Timezone is that daylight saving rules may change on several occasions during a year. But this doesn't
   // matter for our purposes, where we only need to know the approximate time zone to assign the lead to the right person.
   var offsetMins = new Date().getTimezoneOffset();
-  var offsetHrs = offsetMins / 60;
+
+  // Why multiply by -1? Per https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getTimezoneOffset#Description:
+  // The time-zone offset is the difference, in minutes, from local time to UTC. Note that this means that the offset is
+  // positive if the // local timezone is behind UTC and negative if it is ahead. For example, for time zone UTC+10:00
+  // (Australian Eastern Standard Time, Vladivostok Time, Chamorro Standard Time), -600 will be returned.
+  var offsetHrs = (offsetMins / 60) * -1;
 
   $('input#user-utc-timezone-offset').val(offsetHrs);
 
