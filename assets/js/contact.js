71936df599fc62b903b874a29fe0e376bd71abd8
Josh Padnick <josh.padnick@gmail.com>
2019-02-14 13:13:53 -0700

    Simplify the timezone offset.
    Rather than report a nicely formatted timestamp like UTC(0) or UTC+1, if we just report the number itself (e.g. "7") then we can define simple lead assignment rules in Salesforce like [if UTC offset > 2, assign to X, else assign to Y].

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,33 +1,16 @@
 /**
- * Fetch the user's timezone (expressed in UTC) and write it to the contact us form submission. This is useful because
- * we'll use the user's timezone to assign inbound leads.
+ * Fetch the user's timezone offset relative to UTC and write it to the contact us form submission. This is useful because
+ * we'll use an Assignment Rule in Salesforce.com to automatically assign leads based on the offset value.
  */
 
 (function () {
-
-  // Given an offset in mins, return a well-formatted time zone string.
-  //
-  // Example:
-  //   formatOffset(420)
-  //   returns "UTC+7"
-  function getUtcTimezone(offsetMins) {
-    var offsetHrs = offsetMins / 60;
-
-    if (offsetHrs > 0) {
-      return "UTC+" + offsetHrs;
-    } else if (offsetHrs == 0) {
-      return "UTC(0)";
-    } else {
-      return "UTC" + offsetHrs;
-    }
-  }
 
   // Per https://stackoverflow.com/a/34602679, the main limitation of using Javascript's builtin getTimezoneOffset() to
   // calculate Timezone is that daylight saving rules may change on several occasions during a year. But this doesn't
   // matter for our purposes, where we only need to know the approximate time zone to assign the lead to the right person.
   var offsetMins = new Date().getTimezoneOffset();
-  var utcTimeZone = getUtcTimezone(offsetMins);
+  var offsetHrs = offsetMins / 60;
 
-  $('input#user-timezone').val(utcTimeZone);
+  $('input#user-utc-timezone-offset').val(offsetHrs);
 
 }());
