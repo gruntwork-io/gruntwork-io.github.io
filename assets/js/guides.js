b500dc1a530581e65ce1abaa1d722f891d4ed841
Hope Ngerebara <hope.ngerebara@andela.com>
2019-08-27 19:44:46 +0100

    Add fixed scrolling

--- assets/js/guides.js
+++ assets/js/guides.js
@@ -1,12 +1,35 @@
-$(document).ready(function(){
-    $('#newsletter-subscribe').click((e) => {
-        let invalidForm = false;
-        if ($('#email').val() === '') {
-            invalidForm = true;
-            $("#newsletter-success").modal('hide');
-            return;
-        }
-        e.preventDefault();
-        $("#newsletter-success").modal('show');
-    });
+$(document).ready(function () {
+  //Submiting only when subscribe input field not empty. We will update this late to ensure a valid email is entered
+  $('#newsletter-subscribe').click((e) => {
+    let invalidForm = false;
+    if ($('#email').val() === '') {
+      invalidForm = true;
+      $("#newsletter-success").modal('hide');
+      return;
+    }
+    e.preventDefault();
+    $("#newsletter-success").modal('show');
+  });
+
+  //To enable fixed scrolling
+  $(window).scroll(function () {
+    var sidebarTop = $('.navbar-default').innerHeight();
+    var contentHeight = $('.post-section-white').innerHeight();
+    var sidebarHeight = $('#toc').height();
+    var sidebarBottomPos = contentHeight - sidebarHeight;
+    var trigger = $(window).scrollTop() - sidebarTop;
+
+    if ($(window).scrollTop() >= sidebarTop) {
+      $('#toc').addClass('fixed');
+    } else {
+      $('#toc').removeClass('fixed');
+    }
+
+    if (trigger >= sidebarBottomPos) {
+      $('#toc').addClass('bottom');
+    } else {
+      $('#toc').removeClass('bottom');
+    }
+  });
+
 });
