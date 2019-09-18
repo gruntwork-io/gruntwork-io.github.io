26b8035445ff8eea84d9669fe146ea0fca75b729
Hope Ngerebara <hope.ngerebara@andela.com>
2019-09-18 13:27:44 +0100

    Hide page content when success modal is displayed Format guides.js code

--- assets/js/guides.js
+++ assets/js/guides.js
@@ -1,40 +1,44 @@
-$(document).ready(function () {
+$(document)
+  .ready(function () {
 
-  if(window.location.href.indexOf('?') !== -1){
-    $("#newsletter-success").modal('show');
-  }
+    if (window.location.href.indexOf('?') !== -1) {
+      $('#filter-section').hide();
+      $('#guide-listings').hide();
+      $('.guides-newsletter').hide();
+      $("#newsletter-success").modal('show');
+    } 
 
-  //Toggle ToC children
-  $('#toc .sectlevel1 li').click((e) => {
-    // target click comes as <a> inside <li>, hence the parentNode
-    $(e.target.parentNode).toggleClass('expanded');
-  });
+    //Toggle ToC children
+    $('#toc .sectlevel1 li').click((e) => {
+      // target click comes as <a> inside <li>, hence the parentNode
+      $(e.target.parentNode).toggleClass('expanded');
+    });
 
-  const onScroll = function () {
-    const sidebar = $(".js-scroll-with-user");
+    const onScroll = function () {
+      const sidebar = $(".js-scroll-with-user");
 
-    const scrollPosition = $(window).scrollTop();
-    const navBarHeight = $('.navbar-default').innerHeight();
+      const scrollPosition = $(window).scrollTop();
+      const navBarHeight = $('.navbar-default').innerHeight();
 
-    const contentHeight = $('.guides-section-white').innerHeight() + navBarHeight;
-    const sidebarHeight = sidebar.height();
-    const sidebarBottomPos = scrollPosition + sidebarHeight;
+      const contentHeight = $('.guides-section-white').innerHeight() + navBarHeight;
+      const sidebarHeight = sidebar.height();
+      const sidebarBottomPos = scrollPosition + sidebarHeight;
 
-    if (scrollPosition >= navBarHeight) {
-      if (sidebarBottomPos >= contentHeight) {
-        sidebar.removeClass('fixed');
-        sidebar.addClass('bottom');
+      if (scrollPosition >= navBarHeight) {
+        if (sidebarBottomPos >= contentHeight) {
+          sidebar.removeClass('fixed');
+          sidebar.addClass('bottom');
+        } else {
+          sidebar.addClass('fixed');
+          sidebar.removeClass('bottom');
+        }
       } else {
-        sidebar.addClass('fixed');
+        sidebar.removeClass('fixed');
         sidebar.removeClass('bottom');
       }
-    } else {
-      sidebar.removeClass('fixed');
-      sidebar.removeClass('bottom');
-    }
-  };
+    };
 
-  //To enable fixed scrolling
-  $(window).scroll(onScroll);
-  $(onScroll);
-});
+    //To enable fixed scrolling
+    $(window).scroll(onScroll);
+    $(onScroll);
+  });
