76977592d6e93b3aee6e16a36480e393aa2e2f2e
Hope Ngerebara <hope.ngerebara@andela.com>
2019-09-19 17:57:23 +0100

    fix accordion error in console

--- assets/js/main.js
+++ assets/js/main.js
@@ -387,27 +387,28 @@ $(function(){
 });
 // open accordion for direct links to panel id
 $(window).load(function() {
-  var getHash = location.hash; //get hash from js object location
-  function activateAccordion(id) {
-    if (id.length)//check if hash isn't empty
-    {
-      var accordion = $('a[href="' + id + '"]');
-      $panelHeight = accordion.parents('.panel').height();
-      console.log('Offset Top: ' + accordion.parents('.panel').offset().top);
-      $('html,body').animate({
-          scrollTop: accordion.parents('.panel').offset().top //scroll to accordion
-      }, 100, function () {
-          accordion.click(); //simulate click
-          accordion.parents('.panel').find('.panel-title a.no-scroll i.fa').addClass('panel-isOpen'); //point caret down for oepned panel
-      });
-    }
-    else{
-      //if no hash is called, open first panel
-      $('.panel-group .panel:first-child .panel-collapse').addClass('in');
-      $('.panel-group .panel:first-child .panel-title a.no-scroll i.fa').addClass('panel-isOpen');
-    }
-  };
-  activateAccordion(getHash);
+  if(location.pathname.indexOf('/guides') === -1) {
+    var getHash = location.hash; //get hash from js object location
+    function activateAccordion(id) {
+      if (id.length)//check if hash isn't empty
+      {
+        var accordion = $('a[href="' + id + '"]');
+        $panelHeight = accordion.parents('.panel').height();
+        $('html,body').animate({
+            scrollTop: accordion.parents('.panel').offset().top //scroll to accordion
+        }, 100, function () {
+            accordion.click(); //simulate click
+            accordion.parents('.panel').find('.panel-title a.no-scroll i.fa').addClass('panel-isOpen'); //point caret down for oepned panel
+        });
+      }
+      else{
+        //if no hash is called, open first panel
+        $('.panel-group .panel:first-child .panel-collapse').addClass('in');
+        $('.panel-group .panel:first-child .panel-title a.no-scroll i.fa').addClass('panel-isOpen');
+      }
+    };
+    activateAccordion(getHash);
+  }
 });
 
 // Use regular expression to find the cookie we made. Inspired from https://stackoverflow.com/a/25490531
