a407751b7eb94391de2298e63e2fc7ffe8681296
Hope Ngerebara <hope.ngerebara@andela.com>
2019-09-22 13:49:46 +0100

    check panel class exits on page

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,7 +44,7 @@ new function(e){var t=e.separator||"&",s=!1!==e.spaces,n=(e.suffix,!1!==e.prefix
 $(function () {
   if (window.location.href.indexOf('newsletter_success') !== -1) {
     $("#newsletter-success").modal('show');
-  } 
+  }
 });
 
 /* Repo modal link handler */
@@ -391,27 +391,26 @@ $(window).load(function() {
 
   function activateAccordion(id) {
     const accordion = $('a[href="' + id + '"]');
-    const $panelHeight = accordion.parents('.panel').height();
 
-    //Handle cases that do not return a null height
-    if($panelHeight){
-        if (id.length)//check if hash isn't empty
-        {
-          $('html,body').animate({
-              scrollTop: accordion.parents('.panel').offset().top //scroll to accordion
-          }, 100, function () {
-              accordion.click(); //simulate click
-              accordion.parents('.panel').find('.panel-title a.no-scroll i.fa').addClass('panel-isOpen'); //point caret down for oepned panel
-          });
-        }
-        else{
-          //if no hash is called, open first panel
-          $('.panel-group .panel:first-child .panel-collapse').addClass('in');
-          $('.panel-group .panel:first-child .panel-title a.no-scroll i.fa').addClass('panel-isOpen');
-        }
-      };
-    }
-    activateAccordion(getHash);
+    //Check panel class exists 
+    if(accordion.length > 0 && accordion.parents('.panel').length > 0){
+      if (id.length)//check if hash isn't empty
+      {
+        $('html,body').animate({
+            scrollTop: accordion.parents('.panel').offset().top //scroll to accordion
+        }, 100, function () {
+            accordion.click(); //simulate click
+            accordion.parents('.panel').find('.panel-title a.no-scroll i.fa').addClass('panel-isOpen'); //point caret down for oepned panel
+        });
+      }
+      else{
+        //if no hash is called, open first panel
+        $('.panel-group .panel:first-child .panel-collapse').addClass('in');
+        $('.panel-group .panel:first-child .panel-title a.no-scroll i.fa').addClass('panel-isOpen');
+      }
+    };
+  }
+  activateAccordion(getHash);
 });
 
 // Use regular expression to find the cookie we made. Inspired from https://stackoverflow.com/a/25490531
