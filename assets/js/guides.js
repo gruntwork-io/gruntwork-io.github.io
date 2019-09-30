b5e871de8a43c9afec27b9cc262a07b0b0435793
Hope Ngerebara <hope.ngerebara@andela.com>
2019-09-30 13:48:18 +0100

    wip

--- assets/js/guides.js
+++ assets/js/guides.js
@@ -104,6 +104,39 @@ $(document).ready(function () {
     }
   };
 
+  const postDetail = $('.post-detail');
+  // if(postDetail) {
+
+
+  //   // $('.timeline .comment').slice(0, 1).show();
+  // }
+//   $("#loadMore").on('click', function (e) {
+//       e.preventDefault();
+//       $("div:hidden").slice(0, 4).slideDown();
+//       if ($("div:hidden").length == 0) {
+//           $("#load").fadeOut('slow');
+//       }
+//       $('html,body').animate({
+//           scrollTop: $(this).offset().top
+//       }, 1500);
+//   });
+
+
+// $('a[href=#top]').click(function () {
+//     $('body,html').animate({
+//         scrollTop: 0
+//     }, 600);
+//     return false;
+// });
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+        $('.totop a').fadeIn();
+    } else {
+        $('.totop a').fadeOut();
+    }
+});
+
   $(window).scroll(moveToCWithScrolling);
   $(moveToCWithScrolling);
 
