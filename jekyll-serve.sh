a488c37817ba259529ddd33b072895a98511ae8e
Josh Padnick <josh.padnick@gmail.com>
2020-07-23 22:20:01 -0700

    Remove auto-generation of Word Doc version of terms.
    We added this a while ago to allow customers to modify a version of our Terms of Service, but we no longer permit modifications to our standard terms directly, so this doc is no longer needed!

--- jekyll-serve.sh
+++ jekyll-serve.sh
@@ -3,7 +3,4 @@
 set -e
 
 bundle exec jekyll build
-bundle exec pandoc -s --from html _site/terms/word.html --reference-doc=assets/msword/styles.docx -o assets/msword/gruntwork-terms.docx
-echo -e "\e[1;32mA new Terms of Service document (/assets/msword/gruntwork-terms.docx) has been generated."
-echo -e "\e[1;31mRun Jekyll serve to watch for changes"
 bundle exec jekyll serve --livereload --drafts --host 0.0.0.0
