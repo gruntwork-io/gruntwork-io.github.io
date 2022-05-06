8f9e7477bd4ffd159a7c3641b4714f3f34c020ee
Eben Eliason <eben.eliason@gmail.com>
2022-05-06 13:52:24 -0700

    Initial PoC for a Stripe checkout button

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,68 +1,136 @@
 /**
- * Checkout JS using Chargebee.
+ * Checkout JS using Stripe
  *
- * @docs: https://www.chargebee.com/checkout-portal-docs/dropIn.html#chargebee-object
+ * @docs: https://stripe.com/docs/payments/elements
  */
 $(function () {
-  var timeout, $checkout = $('[data-cb-type="checkout"]');
-  var $body = $('body');
+  var timeout,
+    $checkout = $('[data-cb-type="checkout"]');
+  var $body = $("body");
 
   // The checkout state
   var checkoutOptions = {
-    subscription_type: 'standard',
+    subscription_type: "standard",
     pro_support: false,
     setup_deployment: false,
     setup_compliance: false,
-    users: 20
+    products: [],
+    users: 20,
   };
 
-  // Set the UI defaults for Standard AWS selection
-  function _setStandardUIDefaults() {
-    $('#subscription-type-img').attr('data-subscription-type', 'standard');
-    $('#refarch-button-default').show();
-    $('#cis-button-default').show();
+  // Map from addon key to product ID
+  const products = {
+    standard_subscription: "price_1Jx2ZMDJ1uFWlBkUavLabx8m",
+    pro_support: "price_1Ju8buDJ1uFWlBkU0QALdhIM",
+    setup_deployment: "price_1Ju8iYDJ1uFWlBkUxBFauxac",
+    setup_compliance: "price_1Ju8akDJ1uFWlBkUGsewsTJU",
+  };
+
+  // List of selected product IDs
+  var cart = [];
+
+  function _addToCart(addon) {
+    const productID = products[addon];
+    if (!cart.includes(productID)) {
+      cart.push(productID);
+      _updateCart();
+    }
   }
 
-  // Auto toggles the subscription type based on the URI
-  switch ($.query.get('subscription-type')) {
-    case 'standard':
-      _setStandardUIDefaults();
-      _updateCheckout({
-        subscription_type: 'standard'
-      });
-      break;
-    default: // do nothing
+  function _removeFromCart(addon) {
+    const productID = products[addon];
+    cart = cart.filter((item) => item !== productID);
+    _updateCart();
   }
 
-  // Auto toggles addons based on the URI
-  switch ($.query.get('addon')) {
-    case 'subscription-type':
-      _updateCheckout({
-        subscription_type: true
-      });
-      break;
-    case 'setup-deployment':
-      _updateCheckout({
-        setup_deployment: true
-      });
-      break;
-    case 'setup-compliance':
-      _updateCheckout({
-        setup_compliance: true
-      });
-      break;
-    case 'pro-support':
-      _updateCheckout({
-        pro_support: true
-      });
-      break;
-    default: // do nothing
+  function _emptyCart() {
+    cart = [];
+    _updateCart();
   }
-  if (typeof Object.assign != 'function') {
+
+  function _updateCart() {
+    console.log(cart);
+    $("input[name='cart']").val(JSON.stringify(cart));
+    _updateQueryStringFromCart();
+  }
+
+  function _updateQueryStringFromCart() {
+    const addons = [];
+    for (product of cart) {
+      const addon = Object.keys(products).find(
+        (key) => products[key] === product
+      );
+      if (addon && addon != "standard_subscription") {
+        addons.push(addon);
+      }
+    }
+    const query = "?subscription-type=standard&addon=" + addons.join(",");
+    window.history.replaceState(null, "", window.location.pathname + query);
+  }
+
+  function _setDefaults() {
+    // Set the UI defaults for Standard AWS selection
+    $("#subscription-type-img").attr("data-subscription-type", "standard");
+    $("#refarch-button-default").show();
+    $("#cis-button-default").show();
+    _addToCart("standard_subscription");
+
+    // Auto toggles addons based on the URI
+    const addons = $.query.get("addon").split(",");
+    for (const addon of addons) {
+      const addonKey = addon.replace("-", "_");
+      $('.addon-button[name="' + addonKey + '"]').click();
+      _updateCheckout({ [addonKey]: true });
+      _addToCart(addonKey);
+    }
+  }
+
+  // Listen to Addon button clicks
+  $(".addon-button").on("click", function () {
+    var tmp = {};
+    tmp["subscription_type"] = $.query.get("subscription-type");
+    var actionType = $(this).data("addon-action-type");
+
+    // Process the add action and switch the addon button so user can clear their selection
+    function _handleAddAction(button) {
+      tmp[button.name] = true;
+      $(button).text("Remove");
+      $(button).addClass("addon-remove-button");
+      $(button).data("addon-action-type", "Remove");
+      _addToCart(button.name);
+    }
+
+    // Process the remove action and switch the addon button so user can make a selection
+    function _handleRemoveAction(button) {
+      tmp[button.name] = false;
+      $(button)
+        .text("Add to Subscription ")
+        .append(
+          "<i class='fa fa-angle-double-right fa-lg' aria-hidden='true'></i>"
+        );
+      $(button).removeClass("addon-remove-button");
+      $(button).data("addon-action-type", "Add");
+      _removeFromCart(button.name);
+    }
+
+    switch (actionType) {
+      case "Add":
+        _handleAddAction(this);
+        break;
+      case "Remove":
+        _handleRemoveAction(this);
+        break;
+      default: // Do nothing
+    }
+    _updateCheckout(tmp);
+  });
+
+  // Ensure we can assign to an object
+  if (typeof Object.assign != "function") {
     Object.assign = function (target) {
-      'use strict';
+      "use strict";
       if (target == null) {
-        throw new TypeError('Cannot convert undefined or null to object');
+        throw new TypeError("Cannot convert undefined or null to object");
       }
 
       target = Object(target);
@@ -80,47 +148,12 @@ $(function () {
     };
   }
 
-  // Listen to Addon button clicks
-  $('.addon-button').on('click', function () {
-    var tmp = {};
-    tmp["subscription_type"] = $.query.get('subscription-type');
-    var actionType = $(this).data("addon-action-type");
-
-    // Process the add action and switch the addon button so user can clear their selection
-    function _handleAddAction(button) {
-      tmp[button.name] = true;
-      $(button).text("Remove");
-      $(button).addClass('addon-remove-button');
-      $(button).data('addon-action-type', "Remove");
-    }
-
-    // Process the remove action and switch the addon button so user can make a selection
-    function _handleRemoveAction(button) {
-      tmp[button.name] = false;
-      $(button).text("Add to Subscription ").append("<i class='fa fa-angle-double-right fa-lg' aria-hidden='true'></i>");
-      $(button).removeClass('addon-remove-button');
-      $(button).data('addon-action-type', "Add");
-    }
-
-    switch (actionType) {
-      case "Add":
-        _handleAddAction(this);
-        break;
-      case "Remove":
-        _handleRemoveAction(this);
-        break;
-      default: // Do nothing
-    }
-    _updateCheckout(tmp);
-  });
-
   // Updates the UI of the checkout
   function _updateCheckout(newOptions) {
     checkoutOptions = Object.assign({}, checkoutOptions, newOptions);
 
     _updateUI();
     _updatePrice();
-    _updateCheckoutLink();
   }
 
   function _updateUI() {
@@ -128,53 +161,56 @@ $(function () {
     var refarch = checkoutOptions.setup_deployment;
     var compliance = checkoutOptions.setup_compliance;
 
-    $('.grunty-sprite').attr('data-sprite', 0);
-    $('#subscription_type').val(checkoutOptions.subscription_type);
+    $(".grunty-sprite").attr("data-sprite", 0);
+    $("#subscription_type").val(checkoutOptions.subscription_type);
 
     // updates addon switch
-    $('[data-switch]' + '[name="pro_support"]').prop('checked', support);
-    $('[data-switch]' + '[name="setup_deployment"]').prop('checked', refarch);
-    $('[data-switch]' + '[name="setup_compliance"]').prop('checked', compliance);
+    $("[data-switch]" + '[name="pro_support"]').prop("checked", support);
+    $("[data-switch]" + '[name="setup_deployment"]').prop("checked", refarch);
+    $("[data-switch]" + '[name="setup_compliance"]').prop(
+      "checked",
+      compliance
+    );
 
     if (support) {
-      $('.grunty-sprite').attr('data-sprite', 2);
-      $('#subscription-addon-1').removeClass('check-list-disabled');
+      $(".grunty-sprite").attr("data-sprite", 2);
+      $("#subscription-addon-1").removeClass("check-list-disabled");
     } else {
-      $('#subscription-addon-1').addClass('check-list-disabled');
+      $("#subscription-addon-1").addClass("check-list-disabled");
     }
 
     if (refarch) {
-      $('.grunty-sprite').attr('data-sprite', 1);
+      $(".grunty-sprite").attr("data-sprite", 1);
 
-      $('#checkout-price-addon').show();
-      $('#checkout-price-addon--mobile').show().css('display', 'block');
-      $('.checkout-price-view').addClass('move-up');
+      $("#checkout-price-addon").show();
+      $("#checkout-price-addon--mobile").show().css("display", "block");
+      $(".checkout-price-view").addClass("move-up");
 
-      $('#subscription-addon-2').removeClass('check-list-disabled');
+      $("#subscription-addon-2").removeClass("check-list-disabled");
     } else {
-      $('#checkout-price-addon').hide();
-      $('#checkout-price-addon--mobile').hide();
-      $('.checkout-price-view').removeClass('move-up');
+      $("#checkout-price-addon").hide();
+      $("#checkout-price-addon--mobile").hide();
+      $(".checkout-price-view").removeClass("move-up");
 
-      $('#subscription-addon-2').addClass('check-list-disabled');
+      $("#subscription-addon-2").addClass("check-list-disabled");
     }
 
     if (compliance) {
-      $('#subscription-addon-3').removeClass('check-list-disabled');
+      $("#subscription-addon-3").removeClass("check-list-disabled");
 
       // Update Refarch texts to reflect CIS selection
-      $('#subscription-addon-3-text').text('CIS Reference Architecture');
-      $('#addon-text-refarch').text('CIS Reference Architecture');
+      $("#subscription-addon-3-text").text("CIS Reference Architecture");
+      $("#addon-text-refarch").text("CIS Reference Architecture");
     } else {
-      $('#subscription-addon-3').addClass('check-list-disabled');
+      $("#subscription-addon-3").addClass("check-list-disabled");
 
       // Update Refarch texts to reflect CIS selection removal
-      $('#subscription-addon-3-text').text('Reference Architecture');
-      $('#addon-text-refarch').text('Reference Architecture');
+      $("#subscription-addon-3-text").text("Reference Architecture");
+      $("#addon-text-refarch").text("Reference Architecture");
     }
 
     if (support && refarch) {
-      $('.grunty-sprite').attr('data-sprite', 3);
+      $(".grunty-sprite").attr("data-sprite", 3);
     }
   }
 
@@ -182,7 +218,7 @@ $(function () {
     var monthlyTotal, dueNowTotal;
 
     switch (checkoutOptions.subscription_type) {
-      case 'standard':
+      case "standard":
         monthlyTotal = pricing.subscriptions.standard.price.value;
         break;
       default: // do nothing
@@ -190,8 +226,9 @@ $(function () {
 
     if (checkoutOptions.pro_support) {
       switch (checkoutOptions.subscription_type) {
-        case 'standard':
-          monthlyTotal += pricing.subscriptions.standard.pro_support_price.value;
+        case "standard":
+          monthlyTotal +=
+            pricing.subscriptions.standard.pro_support_price.value;
           break;
         default: // do nothing
       }
@@ -205,53 +242,16 @@ $(function () {
 
     if (checkoutOptions.setup_deployment) {
       dueNowTotal += 4950;
-      $('#due-monthly-block').show();
+      $("#due-monthly-block").show();
     } else {
       // only show the monthly disclaimer when it differs from due now
-      $('#due-monthly-block').hide();
+      $("#due-monthly-block").hide();
     }
 
-    $('#due-now').text(dueNowTotal.toLocaleString());
-    $('.monthly-total').text(monthlyTotal.toLocaleString());
+    $("#due-now").text(dueNowTotal.toLocaleString());
+    $(".monthly-total").text(monthlyTotal.toLocaleString());
   }
 
-  // Update the Recurly checkout link URL
-  function _updateCheckoutLink() {
-    const type = checkoutOptions.subscription_type;
-    const support = checkoutOptions.pro_support;
-    const refarch = checkoutOptions.setup_deployment;
-    const compliance = checkoutOptions.setup_compliance;
-
-    var href = "https://gruntwork.recurly.com/subscribe/aws-monthly?";
-
-    // We'll want to pass users when we switch from using the Recurly hosted payment
-    // page to our own, at which point we can re-enable the users add-on.
-
-    const addOns = { "users-monthly": 20 };
-    if (support) {
-      addOns["pro-support-monthly"] = 1;
-    }
-    if (refarch) {
-      addOns["ref-arch"] = 1;
-    }
-    if (compliance) {
-      addOns["cis-aws-foundations-monthly"] = 1;
-    }
-
-    const params = {
-      theme: "modern",
-      add_on_code: Object.keys(addOns).toString(),
-      add_on_quantity: Object.values(addOns).toString(),
-    };
-
-    sep = "";
-    for (const key in params) {
-      href += sep + key + "=" + params[key];
-      sep = "&";
-    }
-
-    $("#recurly-checkout-btn").attr("href", href);
-  }
-
+  _setDefaults();
   _updateCheckout();
 });
